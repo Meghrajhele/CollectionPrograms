d2d0fc7823c7c556e602b630fa0a803b3f904e94
Dell <Dell@Dell-PC>
2018-02-25 22:39:33 +0530

    fc

---
+++ BasicHibernate/src/main/java/com/scp/hb/pojo/Employee.java
@@ -0,0 +1,51 @@
+package com.scp.hb.pojo;
+
+public class Employee {
+	
+	Integer eid;
+	String ename;
+	Double esal;
+	/**
+	 * 
+	 */
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param eid
+	 * @param ename
+	 * @param esal
+	 */
+	public Employee(Integer eid, String ename, Double esal) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.esal = esal;
+	}
+	public Integer getEid() {
+		return eid;
+	}
+	public void setEid(Integer eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Double getEsal() {
+		return esal;
+	}
+	public void setEsal(Double esal) {
+		this.esal = esal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [eid=" + eid + ", ename=" + ename + ", esal=" + esal + "]";
+	}
+	
+	
+
+}
